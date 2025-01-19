09f92bc32e1d659c0708c0f2b7aeea4cc81fce44
mrviularry <91667248+mrviularry@users.noreply.github.com>
2025-01-19 23:19:23 +0000

    Add files via upload

---
+++ AWSEC2-CloudPC.Common/Messages/SetUserStatus.cs
@@ -0,0 +1,12 @@
+﻿using ProtoBuf;
+using AWSEC2-CloudPC.Common.Enums;
+
+namespace AWSEC2-CloudPC.Common.Messages
+{
+    [ProtoContract]
+    public class SetUserStatus : IMessage
+    {
+        [ProtoMember(1)]
+        public UserStatus Message { get; set; }
+    }
+}
